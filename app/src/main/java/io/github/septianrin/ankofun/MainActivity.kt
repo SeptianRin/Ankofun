22da7938bf3e80a18f3f922051f097dc1ea3e526
septianrin <septianrinaldi98@gmail.com>
2021-09-22 10:38:21 +0700

    Final Tutorial Code

--- app/src/main/java/io/github/septianrin/ankofun/MainActivity.kt
+++ app/src/main/java/io/github/septianrin/ankofun/MainActivity.kt
@@ -3,6 +3,7 @@ package io.github.septianrin.ankofun
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -12,31 +13,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //init UI val
         val btnRoll : Button = findViewById(R.id.btn_roll)
-        val btnCountUp : Button = findViewById(R.id.btn_count_up)
         btnRoll.setOnClickListener{rollDice()}
-        btnCountUp.setOnClickListener { countUpDice() }
     }
 
     private fun rollDice() {
+        val ivDice : ImageView = findViewById(R.id.iv_dice)
         val randomInt = (1..6).random()
-        val tvResult : TextView = findViewById(R.id.tv_result)
-        tvResult.text = randomInt.toString()
-        Toast.makeText(this,"Dice Result is ${tvResult.text}", Toast.LENGTH_SHORT).show()
-    }
-
-    private fun countUpDice() {
-        val tvResult : TextView = findViewById(R.id.tv_result)
-        //check if char more than 1 ("Hello World" Case)
-        if(tvResult.text.length>1){
-            tvResult.text = "1"
-        }else{
-            if (tvResult.text == "6"){
-
-            }else{
-                var plus_one = tvResult.text.toString().toInt() +1
-                tvResult.text = plus_one.toString()
-            }
+        val drawableResource = when(randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
-        Toast.makeText(this,"Your Number is ${tvResult.text}", Toast.LENGTH_SHORT).show()
+        ivDice.setImageResource(drawableResource)
     }
 }
